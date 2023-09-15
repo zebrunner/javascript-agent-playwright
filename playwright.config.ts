66adc4c0fe23aed1ef62ef5e8ea40d863c87457d
Anna Sukhodolova <asukhodolova@solvd.com>
2023-09-15 12:16:19 +0300

    0.0.4-dev2: Updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,87 +3,87 @@ import { defineConfig, devices } from '@playwright/test';
 require('dotenv').config();
 
 export default defineConfig({
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  use: {
-    video: 'on',
-    trace: 'on',
-    screenshot: 'only-on-failure',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    use: {
+        video: 'on',
+        trace: 'on',
+        screenshot: 'only-on-failure',
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
-  reporter: [
-    [
-      './src/lib/ZebrunnerReporter.ts',
-      {
-        enabled: true,
-        projectKey: 'DEF',
-        server: {
-          hostname: 'https://test.zebrunner.com',
-          accessToken: 'yourAccessToken'
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
         },
-        launch: {
-          displayName: "Playwright launch",
-          build: '1.0.0',
-          environment: 'Local'
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
         },
-        milestone: {
-          id: null,
-          name: null
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
         },
-        notifications: {
-          notifyOnEachFailure: false,
-          slackChannels: 'dev, qa',
-          teamsChannels: 'dev-channel, management',
-          emails: 'yourEmail@gmail.com'
-        },
-        tcm: {
-          testCaseStatus: {
-            onPass: 'SUCCESS',
-            onFail: 'FAILED',
-          },
-          zebrunner: {
-            pushResults: false,
-            pushInRealTime: false,
-            testRunId: 42
-          },
-          testRail: {
-            pushResults: false,
-            pushInRealTime: false,
-            suiteId: 100,
-            runId: 500,
-            includeAllTestCasesInNewRun: true,
-            runName: 'New Demo Run',
-            milestoneName: 'Demo Milestone',
-            assignee: 'tester@mycompany.com'
-          },
-          xray: {
-            pushResults: false,
-            pushInRealTime: false,
-            executionKey: 'QT-100'
-          },
-          zephyr: {
-            pushResults: false,
-            pushInRealTime: false,
-            jiraProjectKey: 'ZEB',
-            testCycleKey: 'ZEB-T1'
-          }
-        },
-
-        pwConcurrentTasks: 10
-      }
     ],
-  ],
+    reporter: [
+        [
+            '../javascript-agent-playwright',
+            {
+                enabled: true,
+                projectKey: 'DEF',
+                server: {
+                    hostname: 'https://test.zebrunner.com',
+                    accessToken: 'yourAccessToken',
+                },
+                launch: {
+                    displayName: 'Playwright launch',
+                    build: '1.0.0',
+                    environment: 'Local',
+                },
+                milestone: {
+                    id: null,
+                    name: null,
+                },
+                notifications: {
+                    notifyOnEachFailure: false,
+                    slackChannels: 'dev, qa',
+                    teamsChannels: 'dev-channel, management',
+                    emails: 'yourEmail@gmail.com',
+                },
+                tcm: {
+                    testCaseStatus: {
+                        onPass: 'SUCCESS',
+                        onFail: 'FAILED',
+                    },
+                    zebrunner: {
+                        pushResults: false,
+                        pushInRealTime: false,
+                        testRunId: 42,
+                    },
+                    testRail: {
+                        pushResults: false,
+                        pushInRealTime: false,
+                        suiteId: 100,
+                        runId: 500,
+                        includeAllTestCasesInNewRun: true,
+                        runName: 'New Demo Run',
+                        milestoneName: 'Demo Milestone',
+                        assignee: 'tester@mycompany.com',
+                    },
+                    xray: {
+                        pushResults: false,
+                        pushInRealTime: false,
+                        executionKey: 'QT-100',
+                    },
+                    zephyr: {
+                        pushResults: false,
+                        pushInRealTime: false,
+                        jiraProjectKey: 'ZEB',
+                        testCycleKey: 'ZEB-T1',
+                    },
+                },
+
+                pwConcurrentTasks: 10,
+            },
+        ],
+    ],
 });
