fe0433fc8a47afa89a34714974ddc7b06e822647
Dmitry Borisov <36198314+Feuerstarter@users.noreply.github.com>
2024-07-05 16:50:39 +0300

    platform detection: updated userAgent platform with node.js os
    updated Playwright platform with node.js os since Playwright browser userAgent string always points to Windows

--- src/javascript-agent-playwright/helpers/parseBrowserCapabilities.ts
+++ src/javascript-agent-playwright/helpers/parseBrowserCapabilities.ts
@@ -1,8 +1,13 @@
 import UAParser from 'ua-parser-js';
+import * as os from 'os';
 
 export const parseBrowserCapabilities = (launchInfo) => {
   const parser = new UAParser();
   parser.setUA(launchInfo.use.userAgent);
 
-  return parser.getResult();
+  const browserCapabilities = parser.getResult();
+  // rewrite platform from pw userAgent string with node.js os since pw userAgent string always points to Windows
+  browserCapabilities.os.name = os.platform();
+
+  return browserCapabilities;
 };
